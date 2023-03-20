1b7465520f38806471c2014d5a04ae37d5c07e35
DinoCzar <ryanb_booking@yahoo.com>
2023-03-20 15:27:35 -0700

    Add checkWinner function to check if there is a winner and display the result

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 const gameBoard = (() => {
-  const boardValues = ["", "", "", "O", "X", "", "", "", ""];
+  const boardValues = ["X", "", "X", "O", "X", "", "O", "", ""];
+  const playerOneWins = ["X", "X", "X"];
+  const topRow = [boardValues[0], boardValues[1], boardValues[2]];
+  const middleRow = [boardValues[3], boardValues[4], boardValues[5]];
+  const bottomRow = [boardValues[6], boardValues[7], boardValues[8]];
+  const leftColumn = [boardValues[0], boardValues[3], boardValues[6]];
+  const middleColumn = [boardValues[1], boardValues[4], boardValues[7]];
+  const rightColumn = [boardValues[2], boardValues[5], boardValues[8]];
+  const diagonalOne = [boardValues[0], boardValues[4], boardValues[8]];
+  const diagonalTwo = [boardValues[2], boardValues[4], boardValues[6]];
+
   const tile = document.querySelectorAll(".tile");
   tile.forEach((tile) => {
     tile.textContent = boardValues[tile.id - 1];
   });
+
+  const scores = document.querySelector("#scores");
+
+  function checkWinner(input) {
+    if (input[0] === "X" && input[1] === "X" && input[2] === "X") {
+      scores.textContent = "Player 1 Wins!";
+    } else if (input[0] === "O" && input[1] === "O" && input[2] === "O") {
+      scores.textContent = "Player 2 Wins!";
+    }
+  }
+
+  checkWinner(topRow);
+  checkWinner(middleRow);
+  checkWinner(bottomRow);
+  checkWinner(leftColumn);
+  checkWinner(rightColumn);
+  checkWinner(diagonalOne);
+  checkWinner(diagonalTwo);
 })();
