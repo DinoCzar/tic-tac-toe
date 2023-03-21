e04521a4b2eb6e9c42b2a2a9858e700bab798b30
DinoCzar <ryanb_booking@yahoo.com>
2023-03-20 23:02:38 -0700

    Add tie game announcement

--- script.js
+++ script.js
@@ -17,54 +17,70 @@ const gameBoard = (() => {
   tile.forEach((tile) => {
     tile.addEventListener("click", () => {
       if (
-        scores.textContent === "Player 1's Turn" ||
-        scores.textContent === "Player 1 Wins!" ||
-        scores.textContent === "Player 2 Wins!"
+        boardValues[tile.id.slice(7, 8) - 1] === "X" ||
+        boardValues[tile.id.slice(7, 8) - 1] === "O"
       ) {
-        scores.textContent = "Player 2's Turn";
-        boardValues[tile.id.slice(7, 8) - 1] = "X";
+        alert("Please select a blank square");
       } else {
-        scores.textContent = "Player 1's Turn";
-        boardValues[tile.id.slice(7, 8) - 1] = "O";
-      }
+        if (
+          scores.textContent === "Player 1's Turn" ||
+          scores.textContent === "Player 1 Wins!" ||
+          scores.textContent === "Player 2 Wins!" ||
+          scores.textContent === "It's a Tie!"
+        ) {
+          scores.textContent = "Player 2's Turn";
+          boardValues[tile.id.slice(7, 8) - 1] = "X";
+        } else {
+          scores.textContent = "Player 1's Turn";
+          boardValues[tile.id.slice(7, 8) - 1] = "O";
+        }
 
-      tile1.textContent = boardValues[0];
-      tile2.textContent = boardValues[1];
-      tile3.textContent = boardValues[2];
-      tile4.textContent = boardValues[3];
-      tile5.textContent = boardValues[4];
-      tile6.textContent = boardValues[5];
-      tile7.textContent = boardValues[6];
-      tile8.textContent = boardValues[7];
-      tile9.textContent = boardValues[8];
+        tile1.textContent = boardValues[0];
+        tile2.textContent = boardValues[1];
+        tile3.textContent = boardValues[2];
+        tile4.textContent = boardValues[3];
+        tile5.textContent = boardValues[4];
+        tile6.textContent = boardValues[5];
+        tile7.textContent = boardValues[6];
+        tile8.textContent = boardValues[7];
+        tile9.textContent = boardValues[8];
 
-      const topRow = [boardValues[0], boardValues[1], boardValues[2]];
-      const middleRow = [boardValues[3], boardValues[4], boardValues[5]];
-      const bottomRow = [boardValues[6], boardValues[7], boardValues[8]];
-      const leftColumn = [boardValues[0], boardValues[3], boardValues[6]];
-      const middleColumn = [boardValues[1], boardValues[4], boardValues[7]];
-      const rightColumn = [boardValues[2], boardValues[5], boardValues[8]];
-      const diagonalOne = [boardValues[0], boardValues[4], boardValues[8]];
-      const diagonalTwo = [boardValues[2], boardValues[4], boardValues[6]];
+        const topRow = [boardValues[0], boardValues[1], boardValues[2]];
+        const middleRow = [boardValues[3], boardValues[4], boardValues[5]];
+        const bottomRow = [boardValues[6], boardValues[7], boardValues[8]];
+        const leftColumn = [boardValues[0], boardValues[3], boardValues[6]];
+        const middleColumn = [boardValues[1], boardValues[4], boardValues[7]];
+        const rightColumn = [boardValues[2], boardValues[5], boardValues[8]];
+        const diagonalOne = [boardValues[0], boardValues[4], boardValues[8]];
+        const diagonalTwo = [boardValues[2], boardValues[4], boardValues[6]];
 
-      function checkWinner(input) {
-        if (input[0] === "X" && input[1] === "X" && input[2] === "X") {
-          scores.textContent = "Player 1 Wins!";
-          boardValues = ["", "", "", "", "", "", "", "", ""];
-        } else if (input[0] === "O" && input[1] === "O" && input[2] === "O") {
-          scores.textContent = "Player 2 Wins!";
-          boardValues = ["", "", "", "", "", "", "", "", ""];
+        function checkWinner(input) {
+          if (input[0] === "X" && input[1] === "X" && input[2] === "X") {
+            scores.textContent = "Player 1 Wins!";
+            boardValues = ["", "", "", "", "", "", "", "", ""];
+          } else if (input[0] === "O" && input[1] === "O" && input[2] === "O") {
+            scores.textContent = "Player 2 Wins!";
+            boardValues = ["", "", "", "", "", "", "", "", ""];
+          }
+        }
+
+        checkWinner(topRow);
+        checkWinner(middleRow);
+        checkWinner(bottomRow);
+        checkWinner(leftColumn);
+        checkWinner(middleColumn);
+        checkWinner(rightColumn);
+        checkWinner(diagonalOne);
+        checkWinner(diagonalTwo);
+
+        for (let i = 0; i < boardValues.length; i++) {
+          if (boardValues[i] === "X" || boardValues[i] === "O") {
+            break;
+          } else {
+            scores.textContent = "It's a Tie!";
+          }
         }
       }
-
-      checkWinner(topRow);
-      checkWinner(middleRow);
-      checkWinner(bottomRow);
-      checkWinner(leftColumn);
-      checkWinner(middleColumn);
-      checkWinner(rightColumn);
-      checkWinner(diagonalOne);
-      checkWinner(diagonalTwo);
     });
   });
 })();
